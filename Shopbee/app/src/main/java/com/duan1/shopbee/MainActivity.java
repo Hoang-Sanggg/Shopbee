905a3c37265daf9af017a8867f0e5067799bcfb9
HoangSang <phamhoangsang175@gmail.com>
2022-11-10 15:06:50 +0700

    Update firestore

--- Shopbee/app/src/main/java/com/duan1/shopbee/MainActivity.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/MainActivity.java
@@ -1,8 +1,18 @@
 package com.duan1.shopbee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readFireStoreUserStories();
+
+    }
+
+    public void readFireStoreUserStories(){
+        FirebaseFirestore.getInstance()
+                .collection("user")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> snapshots =  queryDocumentSnapshots.getDocuments();
+                        for(DocumentSnapshot snapshot :snapshots){
+                            Log.d("user", "onSuccess: "+snapshot.getString("username"));
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("user", "khong tim ra ");
+                    }
+                });
     }
 }
