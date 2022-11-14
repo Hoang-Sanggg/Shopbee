6dc80c43a325d79941c5c61acf18e5bbdd7666b0
Quangtv208 <100190245+Quangtv208@users.noreply.github.com>
2022-11-15 01:43:47 +0700

    50%

--- Shopbee/app/src/main/java/com/duan1/shopbee/MainActivity.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_profile);
         readFireStoreUserStories();
 
         categoryList = new ArrayList<>();
