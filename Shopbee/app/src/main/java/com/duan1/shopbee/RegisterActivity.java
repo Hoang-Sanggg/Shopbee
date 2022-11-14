d76d23ccfcafe2c1c21d627f077ee55b5585e28e
phudh25tn <117084043+phudh25tn@users.noreply.github.com>
2022-11-14 10:46:58 +0700

    Giao Dien Register, ResetPassWord
    Thuc Code Giao Dien Register, ResetPassWord, giao dien SenOTP, Login OTP chua Custom can dieu chinh lai code.

---
+++ Shopbee/app/src/main/java/com/duan1/shopbee/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.duan1.shopbee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
---
+++ Shopbee/app/src/main/java/com/duan1/shopbee/ResetPasswordActivity.java
@@ -0,0 +1,19 @@
+package com.duan1.shopbee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+    }
+}
