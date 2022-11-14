c5f7d59fa5dac690ff896a32dfeea454c8eb33f1
HoangSang <phamhoangsang175@gmail.com>
2022-11-14 12:11:35 +0700

    Update bottom navigation

--- Shopbee/app/src/main/java/com/duan1/shopbee/MainActivity.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/MainActivity.java
@@ -3,9 +3,13 @@ package com.duan1.shopbee;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.duan1.shopbee.databinding.ActivityMainBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -16,11 +20,43 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         readFireStoreUserStories();
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigation.setItemIconTintList(null);
+
+        binding.bottomNavigation.setItemTextColor(ColorStateList.valueOf(Color.BLACK));
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.bnHome:
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.bnMall:
+                    Toast.makeText(this, "Mall", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.bnLive:
+                    Toast.makeText(this, "Live", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.bnNotification:
+                    Toast.makeText(this, "Notification", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.bnProfile:
+                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        });
 
     }
 
