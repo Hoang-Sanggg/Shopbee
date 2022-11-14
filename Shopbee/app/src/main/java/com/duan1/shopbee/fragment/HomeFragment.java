44db5356e279408bacba223029b4eb4ec2ccf03b
HoangSang <phamhoangsang175@gmail.com>
2022-11-14 18:28:40 +0700

    update home

--- Shopbee/app/src/main/java/com/duan1/shopbee/fragment/HomeFragment.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/fragment/HomeFragment.java
@@ -2,13 +2,22 @@ package com.duan1.shopbee.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.duan1.shopbee.R;
+import com.duan1.shopbee.adapter.CategoryAdapter;
+import com.duan1.shopbee.model.Category;
+
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,9 +29,13 @@ public class HomeFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM1 = "DATA_CATEGORY";
     private static final String ARG_PARAM2 = "param2";
+    private RecyclerView categoryRecycler, newsFeedRecycleView;
 
     // TODO: Rename and change types of parameters
+    private List<Category> categoryList;
+    private CategoryAdapter categoryAdapter;
     private String mParam1;
     private String mParam2;
 
@@ -35,14 +48,17 @@ public class HomeFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
+
      * @param param2 Parameter 2.
      * @return A new instance of fragment HomeFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
+    public static HomeFragment newInstance(List<Category> _categoryList, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
+        args.putSerializable(ARG_PARAM1, (Serializable) _categoryList);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
@@ -53,6 +69,7 @@ public class HomeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
+            categoryList = (List<Category>) getArguments().getSerializable(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
@@ -62,5 +79,16 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        categoryRecycler = view.findViewById(R.id.recyclerCategory);
+        categoryRecycler.setHasFixedSize(true);
+        categoryRecycler.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        categoryAdapter = new CategoryAdapter(categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
---
+++ Shopbee/app/src/main/java/com/duan1/shopbee/function/mFunction.java
@@ -0,0 +1,31 @@
+package com.duan1.shopbee.function;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class mFunction {
+
+    public String BitmapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] b = baos.toByteArray();
+        String temp = Base64.encodeToString(b, Base64.DEFAULT);
+        return  temp;
+    }
+
+    public Bitmap StringBitMap(String encodeToString){
+        try {
+            byte[] encodeByte = Base64.decode(encodeToString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    
+
+}
