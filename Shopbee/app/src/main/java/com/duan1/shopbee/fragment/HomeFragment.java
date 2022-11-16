9737063ea6ba827dc8be67a328027f2de0289a54
Quangtv208 <100190245+Quangtv208@users.noreply.github.com>
2022-11-16 23:46:59 +0700

    Update HomeFragment.java

--- Shopbee/app/src/main/java/com/duan1/shopbee/fragment/HomeFragment.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/fragment/HomeFragment.java
@@ -1,6 +1,9 @@
 package com.duan1.shopbee.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,10 +11,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.duan1.shopbee.R;
 import com.duan1.shopbee.adapter.CategoryAdapter;
