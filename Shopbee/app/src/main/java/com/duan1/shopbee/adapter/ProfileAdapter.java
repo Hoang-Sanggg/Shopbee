094239676cef887da37cbb2ffb6d2f12cdf5176f
Quangtv208 <100190245+Quangtv208@users.noreply.github.com>
2022-11-16 19:01:02 +0700

    hehe

--- Shopbee/app/src/main/java/com/duan1/shopbee/adapter/ProfileAdapter.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/adapter/ProfileAdapter.java
@@ -1,4 +1,60 @@
 package com.duan1.shopbee.adapter;
 
-public class ProfileAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.duan1.shopbee.R;
+import com.duan1.shopbee.function.mFunction;
+import com.duan1.shopbee.model.Category;
+import com.duan1.shopbee.model.Profilel;
+
+import java.util.List;
+
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ProfileHolder> {
+
+    private List<Category> profileList;
+
+    public ProfileAdapter(List<Category> profileList) {
+        this.profileList = profileList;
+    }
+
+    @NonNull
+    @Override
+    public ProfileHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_profile, parent, false);
+        return new ProfileHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProfileHolder holder, int position) {
+        mFunction function = new mFunction();
+        holder.nameProfile.setText(profileList.get(position).getNameCategory());
+        holder.ivAvtProfile.setImageBitmap(function.StringBitMap(profileList.get(position).getImageCategory()));
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return profileList.size();
+    }
+
+    public class ProfileHolder extends RecyclerView.ViewHolder{
+
+
+        private TextView nameProfile;
+        private ImageView ivAvtProfile;
+        public ProfileHolder(@NonNull View itemView) {
+            super(itemView);
+            nameProfile = itemView.findViewById(R.id.profile_reitem);
+            ivAvtProfile = itemView.findViewById(R.id.profile_reimg);
+
+        }
+    }
 }
--- Shopbee/app/src/main/java/com/duan1/shopbee/model/Profilel.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/model/Profilel.java
@@ -3,5 +3,30 @@ package com.duan1.shopbee.model;
 import java.io.Serializable;
 
 public class Profilel implements Serializable {
+    private String nameProfile;
+    private String imgProfile;
 
+    public Profilel() {
+    }
+
+    public Profilel(String nameProfile, String imgProfile) {
+        this.nameProfile = nameProfile;
+        this.imgProfile = imgProfile;
+    }
+
+    public String getNameProfile() {
+        return nameProfile;
+    }
+
+    public void setNameProfile(String nameProfile) {
+        this.nameProfile = nameProfile;
+    }
+
+    public String getImgProfile() {
+        return imgProfile;
+    }
+
+    public void setImgProfile(String imgProfile) {
+        this.imgProfile = imgProfile;
+    }
 }
