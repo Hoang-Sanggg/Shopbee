46486320e6541ec32e340c81526be6e6aa64fb7f
HoangSang <phamhoangsang175@gmail.com>
2022-11-18 10:33:25 +0700

    Revert "Merge remote-tracking branch 'origin/FonterQuang'"
    This reverts commit 0b525fcc8ef30a527ed891394af30b38392fd6d6, reversing
    changes made to 95145d289882ffad8c85a72b105f608ddc1b08fe.

---
+++ Shopbee/app/src/main/java/com/duan1/shopbee/adapter/FlashSaleAdapter.java
@@ -0,0 +1,61 @@
+package com.duan1.shopbee.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.duan1.shopbee.R;
+import com.duan1.shopbee.function.mFunction;
+
+import com.duan1.shopbee.model.Flashsale;
+
+import java.util.List;
+
+public class FlashSaleAdapter extends RecyclerView.Adapter<FlashSaleAdapter.FlashSaleHoder> {
+
+    private List<Flashsale> flashsaleList;
+
+    public FlashSaleAdapter(List<Flashsale> flashsaleList) {
+        this.flashsaleList = flashsaleList;
+    }
+
+    @NonNull
+    @Override
+    public FlashSaleAdapter.FlashSaleHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_flash_sale, parent, false);
+        return new FlashSaleAdapter.FlashSaleHoder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FlashSaleAdapter.FlashSaleHoder holder, int position) {
+        mFunction function = new mFunction();
+        holder.priceFlashSale.setText(flashsaleList.get(position).getPriceFlashSale());
+        holder.soldFlashSale.setText(flashsaleList.get(position).getSoldFlashSale());
+        holder.ivFlashSale.setImageBitmap(function.StringBitMap(flashsaleList.get(position).getImageFlashSale()));
+//        holder.discountFlashSale.setText(flashsaleList.get(position).getDiscountFlashSale());
+    }
+
+    @Override
+    public int getItemCount() {
+        return flashsaleList.size();
+    }
+
+    public class FlashSaleHoder extends RecyclerView.ViewHolder{
+
+
+        private TextView priceFlashSale, discountFlashSale, soldFlashSale;
+        private ImageView ivFlashSale;
+        public FlashSaleHoder(@NonNull View itemView) {
+            super(itemView);
+            priceFlashSale = itemView.findViewById(R.id.tvPriceFlashSale);
+//            discountFlashSale = itemView.findViewById(R.id.);
+            soldFlashSale = itemView.findViewById(R.id.tvSold);
+            ivFlashSale = itemView.findViewById(R.id.ivFlashSale);
+        }
+    }
+}
