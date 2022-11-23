c1b7bad551fd0ec55c8ea081dfaf3624379809a5
HoangSang <phamhoangsang175@gmail.com>
2022-11-23 19:18:58 +0700

    update login and register

---
+++ Shopbee/app/src/main/java/com/duan1/shopbee/model/User.java
@@ -0,0 +1,68 @@
+package com.duan1.shopbee.model;
+
+public class User {
+    private String avartar, username, password, phone, email, gender;
+    private String id;
+
+    public User() {
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User(String password, String username) {
+        this.password = password;
+        this.username = username;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
