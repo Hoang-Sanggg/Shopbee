8cf64fddc68ad42d361a9c3a256167c02f9fdafa
HoangSang <phamhoangsang175@gmail.com>
2022-11-29 22:30:57 +0700

    update profile

--- Shopbee/app/src/main/java/com/duan1/shopbee/fragment/MyProductFragment.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/fragment/MyProductFragment.java
@@ -66,7 +66,7 @@ public class MyProductFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_product2, container, false);
+        return inflater.inflate(R.layout.fragment_my_product, container, false);
     }
 
     @Override
