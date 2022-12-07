ca7ee6be752b811500357fa6dd69c090276a4717
Quangtv208 <100190245+Quangtv208@users.noreply.github.com>
2022-12-07 23:02:45 +0700

    update 100% QR scan

--- Shopbee/app/src/main/java/com/duan1/shopbee/QRscanActivity.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/QRscanActivity.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import com.duan1.shopbee.fragment.HomeFragment;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
 public class QRscanActivity extends AppCompatActivity {
 
     Button btnScan;
+    ImageView btnBack;
 
 
     @Override
@@ -30,11 +34,20 @@ public class QRscanActivity extends AppCompatActivity {
             }
         });
 
+        btnBack = findViewById(R.id.QR_Back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QRscanActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void ScanCode() {
         ScanOptions options = new ScanOptions();
-        options.setPrompt("Volume up to flash on");
+        options.setPrompt("\t Volume up to flash on \n (Hãy bật đèn flash lên để Scan rõ hơn)");
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
