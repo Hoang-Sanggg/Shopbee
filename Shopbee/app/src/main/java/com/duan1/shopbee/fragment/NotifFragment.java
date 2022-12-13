cc3d6104243724039a33c7055ba169078548e0ad
dunghoang-cre <dunghaps1321@gmail.com>
2022-12-13 19:05:44 +0700

    search
    search

--- Shopbee/app/src/main/java/com/duan1/shopbee/fragment/NotifFragment.java
+++ Shopbee/app/src/main/java/com/duan1/shopbee/fragment/NotifFragment.java
@@ -59,6 +59,8 @@ public class NotifFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notif, container, false);
     }
