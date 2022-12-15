ccab43c378bc844629ed48cfde4af8b63abfc46d
phudh25tn <117084043+phudh25tn@users.noreply.github.com>
2022-12-14 23:55:44 -0800

    update

---
+++ Shopbee/app/src/main/java/com/duan1/shopbee/MainActivity.java
@@ -0,0 +1,394 @@
+package com.duan1.shopbee;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.MenuItemCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.duan1.shopbee.callback.HideBottomNav;
+import com.duan1.shopbee.callback.ShowAddProduct;
+import com.duan1.shopbee.callback.ShowBottomNav;
+import com.duan1.shopbee.databinding.ActivityMainBinding;
+import com.duan1.shopbee.fragment.FragmentProduct;
+import com.duan1.shopbee.fragment.HomeFragment;
+import com.duan1.shopbee.fragment.LiveFragment;
+import com.duan1.shopbee.fragment.MallFragment;
+import com.duan1.shopbee.fragment.NotificationFragment;
+import com.duan1.shopbee.fragment.ProfileFragment;
+import com.duan1.shopbee.model.Category;
+import com.duan1.shopbee.model.CategoryMall;
+import com.duan1.shopbee.model.Flashsale;
+import com.duan1.shopbee.model.LiveMain;
+import com.duan1.shopbee.model.LiveStories;
+import com.duan1.shopbee.model.LiveVoucher;
+import com.duan1.shopbee.model.User;
+import com.duan1.shopbee.slide_image.LivePhoto;
+import com.duan1.shopbee.model.ProductCreate;
+import com.duan1.shopbee.slide_image.MallBanner;
+import com.duan1.shopbee.model.Profile;
+import com.duan1.shopbee.slide_image.LivePhoto;
+import com.duan1.shopbee.slide_image.Photo;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements HideBottomNav, ShowBottomNav, ShowAddProduct {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://shopbee-936e3-default-rtdb.firebaseio.com/");
+
+    ActivityMainBinding binding;
+    List<Category> categoryList;
+    List<ProductCreate> flashsaleList;
+
+    List<CategoryMall> categoryMallList;
+    private List<MallBanner> mallBannerList;
+
+    List<LiveStories> liveStoriesList;
+    List<LiveVoucher> liveVoucherList;
+    List<LiveMain> liveMainList;
+    List<LivePhoto> livePhotoList;
+    private List<Photo> listPhoto;
+    private String name;
+    List<Profile> mProfiles;
+
+    LinearLayout toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ///
+
+        Window window = this.getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
+        window.setNavigationBarColor(ContextCompat.getColor(this, R.color.black));
+
+        ///
+
+//        name = getIntent().getStringExtra("username");
+//        if (name == null) {
+//            name = LoginActivity.USERNAME;
+//        }
+        SharedPreferences sharedPref = MainActivity.this.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+        String name = sharedPref.getString("username", "1");
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+
+
+
+        //Home
+        categoryList = new ArrayList<>();
+        flashsaleList = new ArrayList<>();
+        //Live
+        liveMainList = new ArrayList<>();
+        liveStoriesList = new ArrayList<>();
+        liveVoucherList = new ArrayList<>();
+        //profile
+        mProfiles = new ArrayList<>();
+        mProfiles.add(new Profile("hehe", "hehe"));
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigation.getOrCreateBadge(R.id.bnNotification).setNumber(99);
+
+        binding.bottomNavigation.setItemIconTintList(null);
+
+
+        Menu menu = binding.bottomNavigation.getMenu();
+        menu.findItem(R.id.bnHome).setIcon(R.drawable
+                .home_4);
+        menu.findItem(R.id.bnMall).setIcon(R.drawable.shopping_bag);
+        menu.findItem(R.id.bnNotification).setIcon(R.drawable.notification);
+        menu.findItem(R.id.bnLive).setIcon(R.drawable.video_camera);
+        menu.findItem(R.id.bnProfile).setIcon(R.drawable.user);
+
+        binding.bottomNavigation.setItemTextColor(ColorStateList.valueOf(Color.BLACK));
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.bnHome:
+                    item.setIcon(R.drawable.home_4);
+                    menu.findItem(R.id.bnMall).setIcon(R.drawable.shopping_bag);
+                    menu.findItem(R.id.bnNotification).setIcon(R.drawable.notification);
+                    menu.findItem(R.id.bnLive).setIcon(R.drawable.video_camera);
+                    menu.findItem(R.id.bnProfile).setIcon(R.drawable.user);
+                    loadFragmentHome();
+                    break;
+                case R.id.bnMall:
+                    item.setIcon(R.drawable.shopping_bag_2);
+                    menu.findItem(R.id.bnHome).setIcon(R.drawable.home);
+                    menu.findItem(R.id.bnNotification).setIcon(R.drawable.notification);
+                    menu.findItem(R.id.bnLive).setIcon(R.drawable.video_camera);
+                    menu.findItem(R.id.bnProfile).setIcon(R.drawable.user);
+                    loadFragmentMall();
+                    break;
+                case R.id.bnLive:
+                    item.setIcon(R.drawable.video_player_2);
+                    menu.findItem(R.id.bnHome).setIcon(R.drawable.home);
+                    menu.findItem(R.id.bnNotification).setIcon(R.drawable.notification);
+                    menu.findItem(R.id.bnMall).setIcon(R.drawable.shopping_bag);
+                    menu.findItem(R.id.bnProfile).setIcon(R.drawable.user);
+                    loadFragmentLive();
+                    break;
+                case R.id.bnNotification:
+                    item.setIcon(R.drawable.notification_2);
+                    menu.findItem(R.id.bnHome).setIcon(R.drawable.home);
+                    menu.findItem(R.id.bnLive).setIcon(R.drawable.video_camera);
+                    menu.findItem(R.id.bnLive).setIcon(R.drawable.video_camera);
+                    menu.findItem(R.id.bnProfile).setIcon(R.drawable.user);
+                    loadFragmentNotification();
+                    break;
+                case R.id.bnProfile:
+                    item.setIcon(R.drawable.user_2);
+                    menu.findItem(R.id.bnHome).setIcon(R.drawable.home);
+                    menu.findItem(R.id.bnNotification).setIcon(R.drawable.notification);
+                    menu.findItem(R.id.bnLive).setIcon(R.drawable.video_camera);
+                    menu.findItem(R.id.bnNotification).setIcon(R.drawable.notification);
+                    loadFragmentProfile();
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        });
+        readData();
+        loadFragmentHome();
+        readFireStoreCategory();
+        readFireStoreLiveStories();
+        readFireStoreLiveVoucher();
+        readFireStoreLiveMain();
+//        readFireStoreFlashSales();
+//        addData();
+
+    }
+
+    public void loadFragmentHome() {
+        Log.d(">>>", "loadFragmentHome: "+ flashsaleList.size());
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, HomeFragment.newInstance(categoryList, flashsaleList, listPhoto, this, this), "MainFragment")
+                .commit();
+    }
+
+    public void loadFragmentMall() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, MallFragment.newInstance(categoryMallList,flashsaleList, mallBannerList, this, this), "MainFragment")
+                .commit();
+    }
+
+    public void loadFragmentLive() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, LiveFragment.newInstance(liveStoriesList, liveVoucherList, liveMainList,livePhotoList), "LiveFragment")
+                .commit();
+    }
+
+    public void loadFragmentNotification() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, NotificationFragment.newInstance("hehe", "hihi"), "MainFragment")
+                .commit();
+    }
+
+    public void loadFragmentProfile() {
+        Log.d(">>>", "loadFragmentProfile: "+ flashsaleList.size());
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, ProfileFragment.newInstance(mProfiles, flashsaleList, this, this), "MainFragment")
+                .commit();
+    }
+
+
+    public void readFireStoreCategory() {
+        FirebaseFirestore.getInstance()
+                .collection("category")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> snapshots = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot snapshot : snapshots) {
+                            categoryList.add(new Category(snapshot.getString("nameCategory"), snapshot.getString("imageCategory")));
+                        }
+                        loadFragmentHome();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("user", "khong tim ra ");
+                    }
+                });
+    }
+
+    public void readFireStoreFlashSales() {
+        FirebaseFirestore.getInstance()
+                .collection("product")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> snapshots = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot snapshot : snapshots) {
+                                flashsaleList.add(new ProductCreate(snapshot.getString("idProduct"),snapshot.getString("nameProduct"),snapshot.getString("description"),snapshot.getString("industry"),snapshot.getString("priceProduct"),snapshot.getString("productdetail"),snapshot.getString("wirehouse"),snapshot.getString("transportfree"),snapshot.getString("status"),snapshot.getString("nameShop"),snapshot.getString("soldProduct"),snapshot.getString("brandProduct"),snapshot.getString("originProduct"),snapshot.getString("baoHanhSP"),snapshot.getString("shippingProduct"),snapshot.getString("priceFlashSale"),snapshot.getString("discountFlashSale"),snapshot.getString("soldFlashSale"),snapshot.getString("imageProduct")));
+                           // Toast.makeText(MainActivity.this, ""+snapshot.getString("idProduct")+snapshot.getString("description")+snapshot.getString("industry")+snapshot.getString("priceProduct")+snapshot.getString("productdetail")+snapshot.getString("wirehouse")+snapshot.getString("transportfree")+snapshot.getString("status")+snapshot.getString("nameShop")+snapshot.getString("soldProduct")+snapshot.getString("brandProduct")+snapshot.getString("originProduct")+snapshot.getString("baoHanhSP")+snapshot.getString("shippingProduct")+snapshot.getString("priceFlashSale")+snapshot.getString("discountFlashSale")+snapshot.getString("soldFlashSale")+snapshot.getString("imageProduct"), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("user", "khong tim ra ");
+                    }
+                });
+
+
+    }
+
+    public void readFireStoreLiveStories() {
+        FirebaseFirestore.getInstance()
+                .collection("livestories")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> snapshots = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot snapshot : snapshots) {
+                            liveStoriesList.add(new LiveStories(snapshot.getString("imageLiveStories"), snapshot.getString("nameLiveStories")));
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("user", "khong tim ra ");
+                    }
+                });
+
+
+    }
+    public void readFireStoreLiveVoucher() {
+        FirebaseFirestore.getInstance()
+                .collection("livevoucher")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> snapshots = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot snapshot : snapshots) {
+                            liveVoucherList.add(new LiveVoucher(snapshot.getString("imageLiveVoucher"), snapshot.getString("nameLiveVoucher")));
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("user", "khong tim ra ");
+                    }
+                });
+
+
+    }
+    public void readFireStoreLiveMain() {
+        FirebaseFirestore.getInstance()
+                .collection("livemain")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> snapshots = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot snapshot : snapshots) {
+                            liveMainList.add(new LiveMain(snapshot.getString("userimageLiveMain"), snapshot.getString("imageLiveMain"), snapshot.getString("usernameLiveMain"), snapshot.getString("descriptionLiveMain")));
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("user", "khong tim ra ");
+                    }
+                });
+    }
+
+
+    @Override
+    public void hideBottomNav() {
+        toolbar = findViewById(R.id.linearLayout1);
+        binding.bottomNavigation.setVisibility(View.INVISIBLE);
+        toolbar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void showBottomNav() {
+        binding.bottomNavigation.setVisibility(View.VISIBLE);
+        toolbar = findViewById(R.id.linearLayout1);
+        toolbar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showAddProduct() {
+        binding.bottomNavigation.setVisibility(View.VISIBLE);
+    }
+
+
+
+    private void readData(){
+
+        databaseReference.child("product").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                flashsaleList.clear();
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    ProductCreate productCreate = postSnapshot.getValue(ProductCreate.class);
+                    flashsaleList.add(productCreate);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+
+
+}
---
+++ Shopbee/app/src/main/java/com/duan1/shopbee/fragment/ProfileFragment.java
@@ -0,0 +1,170 @@
+package com.duan1.shopbee.fragment;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.duan1.shopbee.LoginActivity;
+import com.duan1.shopbee.R;
+import com.duan1.shopbee.adapter.ProfileAdapter;
+import com.duan1.shopbee.callback.HideBottomNav;
+import com.duan1.shopbee.callback.ShowBottomNav;
+import com.duan1.shopbee.model.ProductCreate;
+import com.duan1.shopbee.model.Profile;
+import com.duan1.shopbee.model.User;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment implements ShowBottomNav{
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "DATA_PROFILE";
+    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView ProfileRecycler;
+
+    private List<Profile> mProfiles;
+    private List<ProductCreate> productCreateList;
+
+    private ProfileAdapter profileAdapter;
+
+    TextView logout;
+
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    ShowBottomNav showBottomNav;
+
+    HideBottomNav hideBottomNav;
+
+    public ProfileFragment(HideBottomNav hideBottomNav, ShowBottomNav showBottomNav) {
+        // Required empty public constructor
+        this.hideBottomNav = hideBottomNav;
+        this.showBottomNav = showBottomNav;
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     *
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProfileFragment newInstance(List<Profile>_mProfiles, List<ProductCreate> _listProduct, HideBottomNav hideBottomNav, ShowBottomNav showBottomNav) {
+        ProfileFragment fragment = new ProfileFragment(hideBottomNav, showBottomNav);
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_PARAM1, (Serializable) _mProfiles);
+        args.putSerializable(ARG_PARAM2, (Serializable) _listProduct);
+//        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mProfiles = (List<Profile>) getArguments().getSerializable(ARG_PARAM1);
+            productCreateList = (List<ProductCreate>) getArguments().getSerializable(ARG_PARAM2);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        ProfileRecycler = view.findViewById(R.id.recyclerProfile);
+//
+//        ProfileRecycler.setHasFixedSize(true);
+//
+//        ProfileRecycler.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+//
+//        mProfiles.size();
+//
+//        profileAdapter = new ProfileAdapter(mProfiles);
+//
+//        ProfileRecycler.setAdapter(profileAdapter);
+
+        logout = view.findViewById(R.id.btn_logout);
+        showBottomNav.showBottomNav();
+        TextView textView = view.findViewById(R.id.btn_myShop);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickMyShop(view);
+                hideBottomNav.hideBottomNav();
+            }
+        });
+        logout = view.findViewById(R.id.btn_logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences sharedPreferences = getContext().getSharedPreferences("LOGIN_STATUS",MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+    }
+
+
+
+    public void onClickMyShop(View view) {
+        requireActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, new MyShopFragment(productCreateList,ProfileFragment.this), "MainFragment")
+                .addToBackStack(null)
+                .commit();
+        hideBottomNav.hideBottomNav();
+    }
+
+    @Override
+    public void showBottomNav() {
+        showBottomNav.showBottomNav();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+}
